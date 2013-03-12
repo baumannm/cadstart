791df7a89c832806811293d5d0134743bdd1aeb8
geierm <martin.geier@opvengineering.de>
2013-03-12 15:09:01 +0100

    set environment to run from command line

---
+++ setpath.bat
@@ -0,0 +1,8 @@
+:: set python path
+
+set PYTHON=YOUR PYTHON DIRECTORY
+
+set PARSERPATH=%PYTHON%\Lib\site-packages\PySide\scripts
+
+set PATH=%PYTHON%;%PYTHON%\Lib\site-packages\PySide;%PYTHON%\Lib\site-packages\PySide\scripts;%PATH%
+
